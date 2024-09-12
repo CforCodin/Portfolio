b5975cb51781fafc74c7367a452a443f19cdfe4a
Aarushi Khatri <98217459+CforCodin@users.noreply.github.com>
2024-09-12 21:27:35 +0530

    Update SocialMediaSidebar.jsx

--- src/components/SocialMediaSidebar/SocialMediaSidebar.jsx
+++ src/components/SocialMediaSidebar/SocialMediaSidebar.jsx
@@ -7,13 +7,13 @@ const SocialMediaSidebar = () => {
       <a href="https://github.com/CforCodin" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
         <FaGithub size={30} className="text-gray-700 hover:text-yellow-500" />
       </a>
-      <a href="https://www.linkedin.com/in/your-linkedin" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
+      <a href="https://www.linkedin.com/in/aarushi-khatri-1b162120b/" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
         <FaLinkedin size={30} className="text-gray-700 hover:text-blue-600" />
       </a>
       <a href="https://twitter.com/your-twitter" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
         <FaTwitter size={30} className="text-gray-700 hover:text-blue-400" />
       </a>
-      <a href="https://instagram.com/your-instagram" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
+      <a href="https://www.instagram.com/s.o.ideas/" target="_blank" rel="noopener noreferrer" className="transform transition-transform duration-300 hover:scale-110">
         <FaInstagram size={30} className="text-gray-700 hover:text-pink-500" />
       </a>
     </div>
